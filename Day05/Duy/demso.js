8abddcc0d309e9c16bd8406d75e8f689d52d4a33
art3mis145 <art3mis145@gmail.com>
2021-01-15 16:16:42 +0700

    duy da them bai

---
+++ Day05/Duy/demso.js
@@ -0,0 +1,43 @@
+const num = [
+  "không",
+  "một",
+  "hai",
+  "ba",
+  "bốn",
+  "năm",
+  "sáu",
+  "bảy",
+  "tám",
+  "chín",
+];
+let a = prompt("Nhập số n:");
+
+function chuc(n) {
+  let result = "";
+  let sohangchuc = Math.floor(n / 10);
+  //   alert(sohangchuc);
+  let donvi = n % 10;
+
+  if (sohangchuc > 1) {
+    result = num[sohangchuc] + " mươi";
+    if (donvi == 1) {
+      result += " mốt";
+    } else if (donvi == 4) {
+      2;
+      result += " tư";
+    } else if (donvi == 5) {
+      result += " lăm";
+    }
+  } else if (sohangchuc == 1) {
+    result += "mười ";
+    if (donvi == 5) {
+      result += " lăm";
+    } else {
+      result += num[donvi];
+    }
+  }
+
+  return result;
+}
+
+alert(chuc(a));
