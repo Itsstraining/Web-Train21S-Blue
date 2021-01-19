1d22702c016e227de45c124f7a2c1d8eeeb6055a
nguyenxuanquang30061999 <quang.nx0628@sinhvien.hoasen.edu.vn>
2021-01-19 16:30:48 +0700

    Day02-W2

---
+++ Day02-W2/Quang/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { ItemComponent } from './components/Item/item.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterComponent,
+    ItemComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
